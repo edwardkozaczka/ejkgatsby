d04ba21e034b09d2b211dc2fbd39c17830e74b01
edwardkozaczka <edwardkozaczka@outlook.com>
2021-05-07 18:01:05 -0400

    changes to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Edward Kozaczka</h1>
+    <p>flexible APPROACH, engaging DELIVERY, dynamic STRATEGY</p>
+    <p>... committed to teaching the WHOLE PERSON</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
