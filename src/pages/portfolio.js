575ac2ed9a1d1876396c6e024c23d6d807047479
edwardkozaczka <edwardkozaczka@outlook.com>
2021-05-07 18:49:29 -0400

    added to layout

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,12 +1,18 @@
-import React from "react"
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 const Portfolio = () => {
-  return (
+  <Layout>
+    <Seo title="Portfolio" /> 
     <div>
       <h1>Teaching Portfolio</h1>
       <p>PLANNING - ENVIRONMENT - INSTRUCTION - DEVELOPMENT</p>
     </div>
-  )
+    <Link to="/">Back to HOME</Link>
+  </Layout>
 }
 
 export default Portfolio
--- src/pages/research.js
+++ src/pages/research.js
@@ -1,12 +1,18 @@
-import React from "react"
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 const Research = () => {
-  return (
+  <Layout>
+    <Seo title="Research" /> 
     <div>
-      <h1>Publications and Recent Talks</h1>
-      <p>WIP</p>
+      <h1>Publications</h1>
+      <p>articles, books, reviews...</p>
     </div>
-  )
+    <Link to="/">Back to HOME</Link>
+  </Layout>
 }
 
 export default Research
